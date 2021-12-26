003ef7b427fddcbb66885ff347697b12217a21d1
tky <takayuki.for.public@gmail.com>
2021-12-26 16:36:10 +0900

    Until creating subnets

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -12,8 +12,23 @@ new NetworkStack(app, 'NetworkStack', {
     cidr: "10.1.0.0/16",
     maxAzs: 2
   },
- publicIngressSubnets : [
+  publicIngressSubnets : [
     { cidrBlock: "10.1.2.0/24", name: "public-ingress-1", availabilityZone: "ap-northeast-2a" },
     { cidrBlock: "10.1.3.0/24", name: "public-ingress-2", availabilityZone: "ap-northeast-2b" },
   ],
+  privateContainerSubnets: [
+    { cidrBlock: "10.1.8.0/24", name: "private-container-1", availabilityZone: "ap-northeast-2a" },
+    { cidrBlock: "10.1.9.0/24", name: "private-container-2", availabilityZone: "ap-northeast-2b" }
+  ],
+  privateDbSubnets: [
+    { cidrBlock: "10.1.16.0/24", name: "private-db-1", availabilityZone: "ap-northeast-2a" },
+    { cidrBlock: "10.1.17.0/24", name: "private-db-2", availabilityZone: "ap-northeast-2b" },
+  ],
+  privateEgressSubnets: [
+    { cidrBlock: "10.1.248.0/24", name: "private-egress-1", availabilityZone: "ap-northeast-2a" },
+    { cidrBlock: "10.1.249.0/24", name: "private-egress-2", availabilityZone: "ap-northeast-2b" }
+  ],
+  publicManageSubnets: [
+    { cidrBlock: "10.1.240.0/24", name: "public-management-1", availabilityZone: "ap-northeast-2a" },
+  ]
 });
--- cdk/lib/network-stack.ts
+++ cdk/lib/network-stack.ts
@@ -6,28 +6,36 @@ import { Construct } from 'constructs';
 interface NetworkProps extends StackProps {
   vpc: { cidr: string, maxAzs: number }
   publicIngressSubnets: { cidrBlock: string, name: string, availabilityZone: string}[]
+  privateContainerSubnets: { cidrBlock: string, name: string, availabilityZone: string}[]
+  privateDbSubnets: { cidrBlock: string, name: string, availabilityZone: string}[]
+  privateEgressSubnets: { cidrBlock: string, name: string, availabilityZone: string}[]
+  publicManageSubnets: { cidrBlock: string, name: string, availabilityZone: string}[]
 }
 
 export class NetworkStack extends Stack {
   public readonly vpc: ec2.IVpc;
   public readonly publicIngressSubnets: ec2.ISubnet[];
+  public readonly privateContainerSubnets: ec2.ISubnet[];
+  public readonly privateDbSubnets: ec2.ISubnet[];
+  public readonly privateEgressSubnets: ec2.ISubnet[];
+  public readonly publicManageSubnets: ec2.ISubnet[];
 
   constructor(scope: Construct, id: string, props: NetworkProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "vpc", {
+    const vpc = new ec2.Vpc(this, "Vpc", {
       cidr: props.vpc.cidr,
       maxAzs: props.vpc.maxAzs,
       subnetConfiguration: []
     });
     this.vpc = vpc;
 
-    const ingressRouteTable = new ec2.CfnRouteTable(this, 'routeIngress', {
+    const ingressRouteTable = new ec2.CfnRouteTable(this, 'RouteTablePublicIngress', {
       vpcId: vpc.vpcId,
       tags: [
         {
           "key": "Name",
-          "value": "route-ingress"
+          "value": "route-public-ingress"
         }
       ],
     });
@@ -68,6 +76,133 @@ export class NetworkStack extends Stack {
         subnetId: sub.ref
       });
       return ec2.Subnet.fromSubnetId(this, p.name, sub.ref);
+    });
+
+    const appRouteTable = new ec2.CfnRouteTable(this, 'RouteTablePrivateContainer', {
+      vpcId: vpc.vpcId,
+      tags: [
+        {
+          "key": "Name",
+          "value": "route-private-container"
+        }
+      ],
+    });
+
+    this.privateContainerSubnets = props.privateContainerSubnets.map(subnet => {
+      const sb = new ec2.CfnSubnet(this, `${subnet.name}-cfnsubnet`, {
+        cidrBlock: subnet.cidrBlock,
+        vpcId: vpc.vpcId,
+        availabilityZone: subnet.availabilityZone,
+        mapPublicIpOnLaunch: false,
+        tags: [
+          {
+            "key": "Name",
+            "value": subnet.name
+          },
+          {
+            "key": "Type",
+            "value": "Isolated"
+          }
+        ],
+      });
+      new ec2.CfnSubnetRouteTableAssociation(this, `${subnet.name}Association`, {
+        routeTableId: appRouteTable.ref,
+        subnetId: sb.ref
+      });
+      return ec2.Subnet.fromSubnetId(this, subnet.name, sb.ref);
+    });
+
+    const dbRouteTable = new ec2.CfnRouteTable(this, 'RouteTableDatabase', {
+      vpcId: vpc.vpcId,
+      tags: [
+        {
+          "key": "Name",
+          "value": "route-private-db"
+        }
+      ],
+    });
+
+    this.privateDbSubnets = props.privateDbSubnets.map(subnet => {
+      const sub = new ec2.CfnSubnet(this, `${subnet.name}-cfnsubnet`, {
+        cidrBlock: subnet.cidrBlock,
+        vpcId: vpc.vpcId,
+        availabilityZone: subnet.availabilityZone,
+        mapPublicIpOnLaunch: false,
+        tags: [
+          {
+            "key": "Name",
+            "value": subnet.name
+          },
+          {
+            "key": "Type",
+            "value": "Isolated"
+          }
+        ],
+      });
+
+      new ec2.CfnSubnetRouteTableAssociation(this, `${subnet.name}Association`, {
+        routeTableId: dbRouteTable.ref,
+        subnetId: sub.ref
+      });
+      return ec2.Subnet.fromSubnetId(this, subnet.name, sub.ref);
+    });
+
+    const egressRouteTable = new ec2.CfnRouteTable(this, 'RouteTablePrivateEgress', {
+      vpcId: vpc.vpcId,
+      tags: [
+        {
+          "key": "Name",
+          "value": "route-private-egress"
+        }
+      ],
+    });
+
+    this.privateEgressSubnets = props.privateEgressSubnets.map(subnet => {
+      const sub =  new ec2.CfnSubnet(this, `${subnet.name}-cfnsubnet`, {
+        cidrBlock: subnet.cidrBlock,
+        vpcId: vpc.vpcId,
+        availabilityZone: subnet.availabilityZone,
+        mapPublicIpOnLaunch: false,
+        tags: [
+          {
+            "key": "Name",
+            "value": subnet.name
+          },
+          {
+            "key": "Type",
+            "value": "Isolated"
+          }
+        ],
+      });
+      new ec2.CfnSubnetRouteTableAssociation(this, `${subnet.name}Association`, {
+        routeTableId: egressRouteTable.ref,
+        subnetId: sub.ref
+      });
+      return ec2.Subnet.fromSubnetId(this, subnet.name, sub.ref);
+    });
+
+    this.publicManageSubnets = props.publicManageSubnets.map(subnet => {
+      const sub = new ec2.CfnSubnet(this, `${subnet.name}-cfnsubnet`, {
+        cidrBlock: subnet.cidrBlock,
+        vpcId: vpc.vpcId,
+        availabilityZone: subnet.availabilityZone,
+        mapPublicIpOnLaunch: true,
+        tags: [
+          {
+            "key": "Name",
+            "value": subnet.name
+          },
+          {
+            "key": "Type",
+            "value": "Public"
+          }
+        ],
+      });
+      new ec2.CfnSubnetRouteTableAssociation(this, `${subnet.name}Association`, {
+        routeTableId: ingressRouteTable.ref,
+        subnetId: sub.ref
+      });
+      return ec2.Subnet.fromSubnetId(this, subnet.name, sub.ref);
     });
   }
 }
